be10e26d4169d5cc5c469aa5922a2d0512ebc34d
painarniano@gmail.com <Gilgamesh555>
2019-10-12 23:16:23 -0400

    iltw

---
+++ utils/valid.js
@@ -0,0 +1,15 @@
+var valid ={
+    checkParams : function(refobj, evalueobj){
+        
+    },
+    checkPassword : function(password){
+
+    },
+    checkEmail : function(email){
+
+    }
+};
+
+valid.checkEmail("tuvieja@gmail.com");
+
+module.exports= valid;
